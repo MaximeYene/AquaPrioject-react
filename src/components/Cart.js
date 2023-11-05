0f40b66de227342270d46f131b082760cd455e14
Maxime Yene <maximeyene@gmail.com>
2023-11-05 23:03:17 +0100

    wip

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -23,7 +23,7 @@ function Cart({ cart, updateCart}) {
 			const {name, price, amount}=cart[i];
 			listeAchats.push(`${name} ${price}Fcfa x ${amount}`);
 		}
-		const queryParams=queryString.stringify({texte:`${intro} ${listeAchats.join('\n')}`});
+		const queryParams=queryString.stringify({`${intro} ${listeAchats.join('\n')}`});
 		const url=`https://wa.me/+237699077977?text=${queryParams}`;
 		window.open(url, '_blank');
 	}
